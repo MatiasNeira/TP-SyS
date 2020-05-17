5ab07a114d2aadc769de8b701c598d9c00dbbee2
Juan Francisco Haidar <juanfhaidar@gmail.com>
2020-05-17 17:26:25 -0300

    Renombramiento
    'Ruidorosa' pasa a ser 'ruidorosa'; 'Xt' pasa a ser 'xT'; 't' pasa a ser 'T'

--- ruidorosa.m
+++ ruidorosa.m
@@ -13,17 +13,17 @@ function xT= ruidorosa(T)
     
     %% Grabacion y Reproduccion del ruido rosa
     
-    bits=16; %bits de grabacion
-    canales = 2; %dos canales para que grabe stereo
-    input= -1; %por default el input del mic en la compu es -1
-    recorder = audiorecorder(Fs,bits,canales,input); %informacion y forma en la que graba
+    bits=16; %bits de grabacion.
+    canales = 2; %dos canales para que grabe stereo.
+    input= -1; %por default el input del mic en la compu es -1.
+    recorder = audiorecorder(Fs,bits,canales,input); %informacion y forma en la que graba.
     
     disp('-----grabando-----')
-    recordblocking(recorder,T); % Graba por un tiempo T
+    recordblocking(recorder,T); % Graba por un tiempo T.
     disp('-----fin de la grabacion-----')
     
-    a=getaudiodata(recorder); %Guarda el audio (en forma matricial) en la variable a
+    a=getaudiodata(recorder); %Guarda el audio (en forma matricial) en la variable a.
     audiowrite('grabacion.wav',a,Fs); % Guarda en forma de archivo de audio .WAV
-    sound(a,Fs) %Reproduce el archivo de audio de la grabacion (a)
+    sound(a,Fs) %Reproduce el archivo de audio de la grabacion (a).
     
 end
