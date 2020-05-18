f81eb2c6e775467d54e8f9429301de86552aa234
Matitom <63811025+Matitom@users.noreply.github.com>
2020-05-18 18:28:17 -0300

    Update inversefilter.m

--- inversefilter.m
+++ inversefilter.m
@@ -1,9 +1,10 @@
-function iF = inversefilter(T,w0,wf,t)
+function iF = inversefilter(T,w0,wf)
 Fs=44100;
 m=Fs*T;
 t=linspace(0,T,m);
+D = flip(SineSweep(T,w0,wf));
 
-iF = modulacion(T,w0,wf,-t).*sinesweep(T,w0,wf,t);
+iF = modulacion(T,wf,w0,t).*D;
 
 
 plot(t,iF);title('Inverse Filter');ylabel('Amplitud');xlabel('Tiempo');
