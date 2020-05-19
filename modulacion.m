82b0e384dec13c5c314c931d1f235d33bb922ec9
Matitom <63811025+Matitom@users.noreply.github.com>
2020-05-19 00:39:18 -0300

    Update modulacion.m

--- modulacion.m
+++ modulacion.m
@@ -1,6 +1,8 @@
 function Mt = modulacion(T,w0,wf,t)
-Fs=44100;
-m=Fs*T;
-t=linspace(0,T,m);
-    Mt=(w0/2*pi*((T*w0)/log(wf/w0))/((T/log(wf/w0))))*exp(t/(T/log(wf/w0)));
+
+    Fs=44100;
+    m=Fs*T;
+    t=linspace(0,T,m);
+    Mt = ((w0)./((((T*w0)/log(wf/w0))/((T/log(wf/w0))))*exp((t)/(T/log(wf/w0)))));
+     
 end
