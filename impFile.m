fb9cefa55b857b7ba07ee762e353e1ca06c2e999
MatiasNeira <63670039+MatiasNeira@users.noreply.github.com>
2020-05-20 12:05:45 -0300

    Creacion Importar Audio

---
+++ impFile.m
@@ -0,0 +1,19 @@
+function [a,b] = impFile(tipo)
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+if tipo == 1
+
+    file = uigetfile('*.wav','Seleccione RI')
+    A=audioread(file);
+    
+end
+
+if tipo == 2
+
+    file = uigetfile('*.wav','Seleccione SS')
+    A =audioread(file);
+    
+    fileb = uigetfile('*.wav','Seleccione FI')
+    B = audioread(fileb);
+end
+end
