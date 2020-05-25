dfc3157b9767417c536a42e94a0ef84f0d3510db
Matitom <63811025+Matitom@users.noreply.github.com>
2020-05-25 19:10:40 -0300

    Update and rename SintRI.m to Sint_RI.m

---
+++ Sint_RI.m
@@ -0,0 +1,34 @@
+function y = Sint_RI(T,Fs,bandas)
+% y = Sint_RI :: Funcion con argumentos de entrada
+% La funcion sintetiza la respuesta al impulso teniendo en cuenta las
+% frecuencias centrales normalizadas segun la banda.
+% Argumentos T,Fs,bandas :: T[segundos];Fs[Hz] 
+%..notas:: T es el tiempo de duracion 
+% Bandas corresponde al filtrado por octava ("oct") o tercio de octava ("tercio").
+% 
+    cant = length(f);
+    T60 = %agregar_correspondinte;
+    y = zeros(1,cant);
+    n = 1/Fs;
+    t = (0:n:T);
+    Y = 0;
+    
+    if bandas == 'oct'
+        f = [125 250 500 1000 2000 4000 8000];
+        else if bandas == 'tercio'
+            f = [125 160 200 250 315 400 500 630 800 1000 1250 1600 2250 3150 4000 5000 6000 8000];
+        end
+    end
+
+
+    for i=(1:cant)
+            for k=(1:length(T60)
+                pi_i(k) = (-log(10^-3))./(T60(k)); 
+            end
+        y(i)=flip(exp(pi_i*t)*cos(2*pi*f(i)*t));
+        Y = Y+y(i);
+    end
+     
+     plot(Y);
+     
+end
