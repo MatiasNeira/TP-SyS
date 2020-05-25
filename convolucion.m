ed155adc4d22a354124cdd4e4be74a4db8753345
Matitom <63811025+Matitom@users.noreply.github.com>
2020-05-25 00:37:22 -0300

    cambie un par de cosas, de parámetros

--- convolucion.m
+++ convolucion.m
@@ -1,4 +1,4 @@
-tf=20; % Tiempo en segundos.
+T=20; % Tiempo en segundos.
 w0=20; %Frecuencia inicial
 wf=2000; %frecuencia Final
 Fs=44100; %frecuencia de muestreo
@@ -6,17 +6,19 @@ m= Fs*tf; %cantidad de muestras
 t = linspace(0,tf,m);
 
 subplot(3,1,1);
-Xt=sineSweep(tf,w0,wf);
-plot(t,Xt);title('Sine-Sweep');ylabel('Amplitud');xlabel('Tiempo')
+
+    Xt=SineSweep(T);
+    plot(t,Xt);title('Sine_Sweep');ylabel('Amplitud');xlabel('Tiempo')
 
 
 subplot(3,1,3);
-Yt=inverseFilter(tf,wf,w0,t);
-plot(t,Yt);title('Inverse Filter');ylabel('Amplitud');xlabel('Tiempo')
+
+    Yt=InverseFilter(T);
+    plot(t,Yt);title('Inverse Filter');ylabel('Amplitud');xlabel('Tiempo')
 
 
 subplot(3,1,3);
-Zt=conv(Xt,Yt);
-plot(Zt);title('Convolucion');ylabel('Amplitud');xlabel('Tiempo')
+    Zt=conv(Xt,Yt);
+    plot(Zt);title('Convolucion');ylabel('Amplitud');xlabel('Tiempo')
 
-audiowrite('Convolucion.wav',Zt,Fs);
+    audiowrite('Convolucion.wav',Zt,Fs);
