3d18e982ca3ecb20b6b3068aa43a0e0a2cbd2d30
Matitom <63811025+Matitom@users.noreply.github.com>
2020-05-25 00:37:49 -0300

    Update convolucion.m

--- convolucion.m
+++ convolucion.m
@@ -1,24 +1,24 @@
-T=20; % Tiempo en segundos.
-w0=20; %Frecuencia inicial
-wf=2000; %frecuencia Final
-Fs=44100; %frecuencia de muestreo
-m= Fs*tf; %cantidad de muestras
+T = 20; % Tiempo en segundos.
+w0 = 20; %Frecuencia inicial
+wf = 2000; %frecuencia Final
+Fs = 44100; %frecuencia de muestreo
+m = Fs*tf; %cantidad de muestras
 t = linspace(0,tf,m);
 
 subplot(3,1,1);
 
-    Xt=SineSweep(T);
+    Xt = SineSweep(T);
     plot(t,Xt);title('Sine_Sweep');ylabel('Amplitud');xlabel('Tiempo')
 
 
 subplot(3,1,3);
 
-    Yt=InverseFilter(T);
+    Yt = InverseFilter(T);
     plot(t,Yt);title('Inverse Filter');ylabel('Amplitud');xlabel('Tiempo')
 
 
 subplot(3,1,3);
-    Zt=conv(Xt,Yt);
+    Zt = conv(Xt,Yt);
     plot(Zt);title('Convolucion');ylabel('Amplitud');xlabel('Tiempo')
 
     audiowrite('Convolucion.wav',Zt,Fs);
