cb07a4bccff3702e899417791670ea01e654fe6e
Juan Francisco Haidar <juanfhaidar@gmail.com>
2020-05-31 17:45:52 -0300

    Update import_data.m

--- import_data.m
+++ import_data.m
@@ -1,5 +1,14 @@
 function Import = import_data()
 
+%                  ...
+
+% Import=import_data()::Funcion sin elementos de entrada
+% Funcion que importa archivos ´.wav´ seleccionados y recopila sus correspodientes datos de forma numerica
+% .RespuestaImpulso :: Importa la RI seleccionada y recopila sus datos
+% .SS_FI :: Importa el sine-sweep y el filtro inverso seleccionados y recopila sus dato
+
+%                  ...
+
 
 disp('----Elija las caracteristicas del archivo----')
 
