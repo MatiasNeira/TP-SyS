5a4dd037e73ec9b9eec8dcda4d17376eada8289c
Juan Francisco Haidar <juanfhaidar@gmail.com>
2020-05-31 19:48:05 -0300

    Update Modulacion.m

--- Modulacion.m
+++ Modulacion.m
@@ -1,12 +1,17 @@
 function Mt = Modulacion(T,F0,Ff)
-%Mt = Modulacion :: Funcion con argumento de entrada.
-% Argumentos : T [segundos] tiempo de duracion.
-%              F0 [Hz] frecuencia inicial.
-%              Ff [Hz] frecuencia final.
-%La funcion realiza una modulacion de compensacion 
-%de eneria para el filtro inverso.
-%procesando los datos : w0,wf,Fs [Hz] y t (array). Dando la salida Mt.
-% ..nota:: t es un array con "m" puntos entre 0 y T.
+
+%                    ...
+% 
+% Mt = Modulacion :: Funcion con argumento de entrada
+% Argumentos : T [segundos] tiempo de duracion
+%              F0 [Hz] frecuencia inicial
+%              Ff [Hz] frecuencia final
+% La funcion realiza una modulacion de compensacion 
+% de eneria para el filtro inverso
+% Procesando los datos : w0,wf,Fs [Hz] y t (array)
+% ..nota:: t es un array con "m" puntos entre 0 y T
+%
+%                    ...
     
     w0 = 2*pi*F0;
     wf = 2*pi*Ff;
