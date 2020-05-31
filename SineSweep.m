1d7bbdd97e509a573be7c3a5125cfaf3649bd25d
Juan Francisco Haidar <juanfhaidar@gmail.com>
2020-05-31 17:51:19 -0300

    Update SineSweep.m

--- SineSweep.m
+++ SineSweep.m
@@ -1,11 +1,17 @@
 function Xt = SineSweep(T,F0,Ff)
-%Xt = SineSweep :: Funcion con argumento de entrada.
-% Argumentos : T [segundos] tiempo de duracion.
-%              F0 [Hz] frecuencia inicial.
-%              Ff [Hz] frecuencia final.
-%La funcion realiza un Sinesweep de tiempo T asignado como entrada
-%procesando los datos : w0,wf,Fs [Hz] y t (array). Dando la salida Xt.
+
+%                   ...
+%
+% Xt=SineSweep :: Funcion con argumentos de entrada
+% Argumentos : T [segundos] tiempo de duracion
+%              F0 [Hz] frecuencia inicial
+%              Ff [Hz] frecuencia final
+% La funcion realiza un sine-sweep de tiempo T asignado,
+% procesando los datos: w0, wf, Fs [Hz] y t. La salida es Xt.
 % ..nota:: t es un array con "m" puntos entre 0 y T.
+%
+%                   ...
+
     
     w0 = 2*pi*F0;
     wf = 2*pi*Ff;
