9849649a508689cae8766fb16023e04acd9f66ae
Juan Francisco Haidar <juanfhaidar@gmail.com>
2020-06-01 17:06:17 -0300

    Update IntSch.m

--- IntSch.m
+++ IntSch.m
@@ -1,13 +1,15 @@
 function [E] = IntSch(RI,Time)
-%                  ...
 
-% [E] = IntSch(RI,T)::Funcion con elementos de entradas : RI (Respuesta al
-% impulso) --- Time tiempo [segundos]
+%                     ...
+%
+% [E] = IntSch(RI,T)::Funcion con elementos de entradas :
+%     RI :: Respuesta al impulso
+%     Time :: tiempo [segundos]
 % Funcion que aproxima un impulso a una señal mas adecuada, a traves de la
 % transformada de Schroeder.
 % Salida E : array de valores.
-
-%                  ...
+%
+%                    ...
 
 Int_arg = RI^2;
 t =(0:Time);
