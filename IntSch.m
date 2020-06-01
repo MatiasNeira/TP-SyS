9636cff93bdd92a156816f8a28e353fdb78bb8fa
Matitom <63811025+Matitom@users.noreply.github.com>
2020-06-01 16:59:24 -0300

    Update IntSch.m

--- IntSch.m
+++ IntSch.m
@@ -1,10 +1,21 @@
-function [E] = IntSch(RI,T)
-%UNTITLED10 Summary of this function goes here
-%   Detailed explanation goes here
-a=RI^2;
-t=(0:T);
-for i_(0:T)
-E(i)= integral(a,0,inf) - integral(a,0,t(i));
-end
+function [E] = IntSch(RI,Time)
+%                  ...
+
+% [E] = IntSch(RI,T)::Funcion con elementos de entradas : RI (Respuesta al
+% impulso) --- Time tiempo [segundos]
+% Funcion que aproxima un impulso a una señal mas adecuada, a traves de la
+% transformada de Schroeder.
+% Salida E : array de valores.
+
+%                  ...
+
+Int_arg = RI^2;
+t =(0:Time);
+E = zeros(1,Time);
+
+    for i = (0:Time)
+        E(i)= integral(Int_arg,0,inf) - integral(Int_arg,0,t(i));
+    end
+
 end
 
