9428e5e33aa99371d7a8b6b1c92ca7b1f5f0f6b8
Juan Francisco Haidar <juanfhaidar@gmail.com>
2020-06-12 00:01:53 -0300

    Update RL_CM.m

--- RL_CM.m
+++ RL_CM.m
@@ -1,29 +1,33 @@
 function Y = RL_CM(a)
 
-%Aca va el Docstring
+%
+%                        ...
+% Funcion que aproxima una recta mediante una regresion lineal con un ajuste por cuadrados minimos
+% Parametros de entrada :: 
+%                           a: 
+%                        ...
 
-n=length(a);
-x=linspace(0,1,n);
 
-E=0;
-Ex=0;
-Ey=0;
+    n=length(a);
+    x=linspace(0,1,n);
+    E=0;
+    Ex=0;
+    Ey=0;
 
-for i=1:n
-    E=E+(a(i)*i);
-    Ex=Ex+i;
-    Ey=Ey+a(i);
-end
+    for i=1:n
+        E=E+(a(i)*i);
+        Ex=Ex+i;
+        Ey=Ey+a(i);
+    end
 
-m=(n*E-Ex*Ey)/(n*(Ex)^2 - abs((Ex)^2));
-b=(Ey*(Ex)^2 -Ex*E)/(n*(Ex)^2 - abs((Ex)^2));
+    m=(n*E-Ex*Ey)/(n*(Ex)^2 - abs((Ex)^2));
+    b=(Ey*(Ex)^2 -Ex*E)/(n*(Ex)^2 - abs((Ex)^2));
+    Y=m*x+b;
 
-Y=m*x+b;
-
-subplot (2,1,1)
-plot(a)
-subplot(2,1,2)
-plot(Y)
-end
+    subplot (2,1,1)
+    plot(a)
+    subplot(2,1,2)
+    plot(Y)
+    end
 
 
