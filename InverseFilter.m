a1776f45f7a8d58a577804ea1bd008155ba95737
Matitom <63811025+Matitom@users.noreply.github.com>
2020-06-14 20:58:50 -0300

    Update InverseFilter.m

--- InverseFilter.m
+++ InverseFilter.m
@@ -9,16 +9,22 @@ function iF = InverseFilter(T,F0,Ff)
 % La funcion realiza un filtro inverso de tiempo T [segundos]
 % 
 %                      ...
+    Fs=44100;
+    m=Fs*T;
+    t=linspace(0,T,m);
 
+    D = flip(SineSweep(T,F0,Ff));
+
+    iF = Modulacion(T,F0,Ff).*D;
+
+% Test
+    %Test_INvFilt(t,iF) % Descomentar para testear
+
+end
+
+function Test = Test_INvFilt(t,iF)
+% --- Test para el filtro inverso :: Entrega una representacion grafica de Amplitud vs Tiempo ---
+
+    plot(t,iF);title('Inverse Filter');ylabel('Amplitud');xlabel('Tiempo');
     
-Fs=44100;
-m=Fs*T;
-t=linspace(0,T,m);
-
-D = flip(SineSweep(T,F0,Ff));
-
-iF = Modulacion(T,F0,Ff).*D;
-
-
-plot(t,iF);title('Inverse Filter');ylabel('Amplitud');xlabel('Tiempo');
 end
