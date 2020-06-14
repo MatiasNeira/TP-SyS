e193241133e358280b471781591b09403f878993
Matitom <63811025+Matitom@users.noreply.github.com>
2020-06-14 20:57:23 -0300

    Update Hilbert.m

--- Hilbert.m
+++ Hilbert.m
@@ -1,13 +1,13 @@
 function soft_signal = Hilbert(original_signal)
 %                  ...
 
-% soft_signal = Hilbert(original_signal)::Funcion con elemento de entrada original_signal [array de valores de una señal]
-% Funcion que suaviza una señal a traves de la transformadad de Hilbert.
-% Salida soft_signal :: array de valores de la señal suavizada.
+% soft_signal = Hilbert(original_signal)::Funcion con elemento de entrada original_signal [array de valores de una seÃ±al original]
+% Funcion que suaviza una seÃ±al a traves de la transformadad de Hilbert.
+% Salida soft_signal :: array de valores de la seÃ±al suavizada.
 %                  ...
 
-t = (0:1:length(original_signal));
-
-soft_signal = conv(original_signal,1/(pi*t));
-
+    transformada = hilbert(original_signal);
+    
+    soft_signal = abs(transformada);
+    
 end
