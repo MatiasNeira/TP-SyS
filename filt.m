abe6598fd25257a280e850ab71ecd9e444c93ba8
Matitom <63811025+Matitom@users.noreply.github.com>
2020-06-14 21:03:15 -0300

    IMPORTANTE. cambie muchas cosas, ahora va mejor

--- filt.m
+++ filt.m
@@ -20,28 +20,64 @@ function filtros = filt()
    
 end
 
-function octaveFilterBank = Octava()
- 
-    centerFrequencies = [16 31 63 125 250 500 1000 2000 4000 8000 16000];
-
-    for i=1:11
-        octaveFilterBank{i} = octaveFilter(centerFrequencies(i),'1 octave','FilterOrder', 6);
-    end
-
-    plotter=fvtool(getFilter(octaveFilterBank{1}),getFilter(octaveFilterBank{2}),getFilter(octaveFilterBank{3}),getFilter(octaveFilterBank{4}),getFilter(octaveFilterBank{5}),getFilter(octaveFilterBank{6}),getFilter(octaveFilterBank{7}),getFilter(octaveFilterBank{8}),getFilter(octaveFilterBank{9}),getFilter(octaveFilterBank{10}),getFilter(octaveFilterBank{11}));
-    set(plotter,'FrequencyScale','Log');
-
-end
+function z = Octava(Audio)
     
-function octaveFilterBank = Tercio()
-
-    centerFrequencies= [12.5 16 20 25 31.5 40 50 63 80 100 125 160 200 250 315 400 500 630 800 1000 1250 1600 2000 2500 3150 4000 5000 6300 8000 10000 12500 16000 20000];
+    Fs = 44100;
     
-    for i=1:33
-       octaveFilterBank{i} = octaveFilter(centerFrequencies(i),'FilterOrder',8);
+    fc = [16 31 63 125 250 500 1000 2000 4000 8000];
+    
+    octaveFilterBank = cell(1,length(fc));
+    
+    for i=1:10
+        
+            f_in = fc(i)/2^(1/2);
+            
+            f_sup = fc(i)*2^(1/2);
+            
+            octaveFilterBank{1,i} = designfilt('bandpassiir','FilterOrder',8,'HalfPowerFrequency1',f_in,'HalfPowerFrequency2',f_sup,'SampleRate',Fs);
+            
     end
     
-    plotter = fvtool(getFilter(octaveFilterBank{1}),getFilter(octaveFilterBank{2}),getFilter(octaveFilterBank{3}),getFilter(octaveFilterBank{4}),getFilter(octaveFilterBank{5}),getFilter(octaveFilterBank{6}),getFilter(octaveFilterBank{7}),getFilter(octaveFilterBank{8}),getFilter(octaveFilterBank{9}),getFilter(octaveFilterBank{10}),getFilter(octaveFilterBank{11}),getFilter(octaveFilterBank{12}),getFilter(octaveFilterBank{13}),getFilter(octaveFilterBank{14}),getFilter(octaveFilterBank{15}),getFilter(octaveFilterBank{16}),getFilter(octaveFilterBank{17}),getFilter(octaveFilterBank{18}),getFilter(octaveFilterBank{19}),getFilter(octaveFilterBank{20}),getFilter(octaveFilterBank{21}),getFilter(octaveFilterBank{22}),getFilter(octaveFilterBank{23}),getFilter(octaveFilterBank{24}),getFilter(octaveFilterBank{25}),getFilter(octaveFilterBank{26}),getFilter(octaveFilterBank{27}),getFilter(octaveFilterBank{28}),getFilter(octaveFilterBank{29}),getFilter(octaveFilterBank{30}),getFilter(octaveFilterBank{31}),getFilter(octaveFilterBank{32}),getFilter(octaveFilterBank{33}));
+%         plotter = fvtool(octaveFilterBank{1},octaveFilterBank{2},octaveFilterBank{3},octaveFilterBank{4},octaveFilterBank{5},octaveFilterBank{6},octaveFilterBank{7},octaveFilterBank{8},octaveFilterBank{9},octaveFilterBank{10});
+%         set(plotter,'FrequencyScale','Log');
+     
+    z = {zeros(1,length(Audio))};
+
+    for i = 1:length(octaveFilterBank)
+
+        fil = octaveFilterBank{i};
+
+        z = filtfilt(fil,Audio);
+
+    end
+end
+    
+function z = Tercio(Audio)
+
+    Fs=44100;
+    fc=[12.5 16 20 25 31.5 40 50 63 80 100 125 160 200 250 315 400 500 630 800 1000 1250 1600 2000 2500 3150 4000 5000 6300 8000 10000 12500 16000 20000];
+        
+    octaveFilterBank = cell(1,length(fc));
+    
+        for i = 1:26
+            
+            f_in = fc(i)/2^(1/2);
+            f_sup = fc(i)*2^(1/2);
+            
+            octaveFilterBank{1,i} = designfilt('bandpassiir','FilterOrder',8,'HalfPowerFrequency1',f_in,'HalfPowerFrequency2',f_sup,'SampleRate',Fs);  
+        end
+    
+    plotter = fvtool(octaveFilterBank{1},octaveFilterBank{2},octaveFilterBank{3},octaveFilterBank{4},octaveFilterBank{5},octaveFilterBank{6},octaveFilterBank{7},octaveFilterBank{8},octaveFilterBank{9},octaveFilterBank{10},octaveFilterBank{11},octaveFilterBank{12},octaveFilterBank{13},octaveFilterBank{14},octaveFilterBank{15},octaveFilterBank{16},octaveFilterBank{17},octaveFilterBank{18},octaveFilterBank{19},octaveFilterBank{20},octaveFilterBank{21},octaveFilterBank{22},octaveFilterBank{23},octaveFilterBank{24},octaveFilterBank{25},octaveFilterBank{26},octaveFilterBank{27},octaveFilterBank{28},octaveFilterBank{29},octaveFilterBank{30},octaveFilterBank{31},octaveFilterBank{32},octaveFilterBank{33});
     set(plotter,'FrequencyScale','Log');
     
+    z = {zeros(1,length(Audio))};
+
+        for i = 1:length(octaveFilterBank)
+
+            fil = octaveFilterBank{i};
+
+            z = filtfilt(fil,Audio);
+
+        end
 end
+
