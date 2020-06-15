5567594bee40c0f95b6fa007b8e91c19b71571c2
Juan Francisco Haidar <juanfhaidar@gmail.com>
2020-06-15 12:41:05 -0300

    Update parametros.m

--- parametros.m
+++ parametros.m
@@ -2,10 +2,11 @@ function [T60,EDT,D50,C80] = parametros(A,Fs)
 
 %                  ...
 
-% [T60,EDT,D50,C80] = parametros(A,Fs)::Funcion con elementos de entradas A
-% [señal de entrada(Normalizada)] y Fs [Hz] frecuencia de muestreo.
 % Funcion que calcula parametros acusticos segun la norma señalada.
-% Salida [T60,EDT,D50,C80] : Parametros calculados entregados en un array.
+% Funcion con elementos de entrada: 
+%            A = [señal de entrada(Normalizada)]
+%            Fs = frecuencia de muestreo [Hz].
+% Salida = [T60,EDT,D50,C80] = Parametros calculados entregados en un array.
 
 %                  ...
 
