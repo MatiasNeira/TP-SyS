cbc708bd2eda584e80a32cd16c2960440ba8fa4a
MatiasNeira <63670039+MatiasNeira@users.noreply.github.com>
2020-06-15 15:34:53 -0300

    Update Sint_RI.m

--- Sint_RI.m
+++ Sint_RI.m
@@ -6,26 +6,25 @@ function y = Sint_RI(T,Fs,bandas,T60)
 %..notas:: T es el tiempo de duracion 
 % Bandas corresponde al filtrado por octava ("oct") o tercio de octava ("tercio").
 % 
-    
-    
+    cant = length(f);
+    y = zeros(1,cant);
     n = 1/Fs;
     t = (0:n:T);
     Y = 0;
-    f=0;
+    
     if bandas == 'oct'
         f = [125 250 500 1000 2000 4000 8000];
         else if bandas == 'tercio'
             f = [125 160 200 250 315 400 500 630 800 1000 1250 1600 2250 3150 4000 5000 6000 8000];
         end
     end
-    cant = length(f);
-    y = zeros(1,cant);
-    a = zeros(1,cant);
-    pi_i = (-log(10^-3))/(T60); 
-    
+
+
     for i=(1:cant)
-       a(i)=f(i)*(2*pi*t(i));
-   y(i)=flip(exp(pi_i*t(i))*cos(a(i)));
+            for k=(1:length(T60)
+                pi_i(k) = (-log(10^-3))./(T60(k)); 
+            end
+        y(i)=flip(exp(pi_i*t)*cos(2*pi*f(i)*t));
         Y = Y+y(i);
     end
      
